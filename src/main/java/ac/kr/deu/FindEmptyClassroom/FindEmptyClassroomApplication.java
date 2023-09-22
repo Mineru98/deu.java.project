47777b522e22fee5e7684381c16d48442186d9f8
iamiks <rmstjr1030@naver.com>
2023-09-22 18:47:32 +0900

    Feat: 프로젝트 초기 설정

---
+++ src/main/java/ac/kr/deu/FindEmptyClassroom/FindEmptyClassroomApplication.java
@@ -0,0 +1,13 @@
+package ac.kr.deu.FindEmptyClassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindEmptyClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindEmptyClassroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/kr/deu/FindEmptyClassroom/FindEmptyClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.deu.FindEmptyClassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindEmptyClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
